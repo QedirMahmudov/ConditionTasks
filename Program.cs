ba3da6f90b845ff5025ace96a7caaee9400b7ec9
QedirMahmudov <1qedirmahmudov@gmail.com>
2025-03-10 23:11:54 +0400

    ConditionTasks

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿//1ci ve 2ci sualda ededi Console.ReadLine(); ile daxil etmey isteyirikse, Convert.ToInt32(Console.ReadLine()); ile eded daxil etmeyimiz kifayetdr.
+
+//Sual 1--------------------------------------------------
+//int num = 22;
+
+//int quotientNum = num / 10;
+//int remainderNum = num % 10;
+//if (quotientNum > remainderNum)
+//{
+//    Console.WriteLine($"Ededin tam hissesi daha boyukdur:{quotientNum}");
+//}
+//else if (quotientNum == remainderNum)
+//{
+//    Console.WriteLine("Ededin tam ve qaliq hissesi bir birine beraberdir");
+//}
+//else
+//{
+//    Console.WriteLine($"Ededin qaliq hissesi daha boyukdur:{remainderNum}");
+
+//}
+
+
+
+//Sual 2---------------------------------------------------
+
+//int num = 265;
+//int quotientNum = num / 10;
+//int indexTwo = num % 10;
+//if (quotientNum > 10)
+//{
+//    int indexZero = quotientNum / 10;
+//    int indexOne = quotientNum % 10;
+//    Console.WriteLine(indexZero+indexOne+indexTwo);
+//}
+
+
+
+
+
+//Sual 3------------------------------------------------
+//char symbol = Console.ReadLine()[0];
+
+
+//switch (symbol)
+//{
+//    case 'A':
+//        Console.WriteLine("AZ");
+//        break;
+//    case 'a':
+//        Console.WriteLine("Azərbaycan dili seçildi");
+//        break;
+//    case 'R':
+//        Console.WriteLine("RU");
+//        break;
+//    case 'r':
+//        Console.WriteLine("Rus dili seçildi");
+//        break;
+//    default:
+//        Console.WriteLine("Yanlış sorğu");
+//        break;
+//}
